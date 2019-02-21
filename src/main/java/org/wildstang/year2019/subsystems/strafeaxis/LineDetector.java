4bdcfa8e04c41c745e3cb1cc8ac59299003b603d
Wuump <39094532+Wuump@users.noreply.github.com>
2019-02-20 18:38:09 -0600

    Changes to Strafe Axis and Line Follow

--- src/main/java/org/wildstang/year2019/subsystems/strafeaxis/LineDetector.java
+++ src/main/java/org/wildstang/year2019/subsystems/strafeaxis/LineDetector.java
@@ -1,15 +1,43 @@
 /* Placeholder --- no line detector code yet. This might be unnecessary as line detection might happen on RasPi instead.*/
 
 package org.wildstang.year2019.subsystems.strafeaxis;
+import javax.sound.sampled.Line;
+
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 
 public class LineDetector {
-    private SerialPort arduino = new SerialPort(9600, Port.kUSB);
+    private SerialPort arduino; ;
     private byte[] lineLocation = new byte[1]; 
     
+    public LineDetector() {
+        try {
+            arduino = new SerialPort(9600, Port.kUSB);  
+            System.out.println("Connected to kUSB");
+          }
+          catch(Exception e) {
+            System.out.println("Falied to connect to kUSB.  Attempting to connect to kUSB1");
+            try {
+              arduino = new SerialPort(9600, Port.kUSB1);  
+              System.out.println("Connected to kUSB1");
+            }
+            catch(Exception e1) {
+              System.out.println("Falied to connect to kUSB1.  Attempting to connect to kUSB2");
+              try {
+                arduino = new SerialPort(9600, Port.kUSB2);  
+                System.out.println("Connected to kUSB2");
+              }
+              catch(Exception e2) {
+                System.out.println("Failed to connect");
+              }
+            }
+          }
+          arduino.setReadBufferSize(1);
+    }
+
     public double getLinePosition() {
-        lineLocation = arduino.read(0);
+        lineLocation = arduino.read(1);
+        System.out.println(lineLocation[0]);
         return lineLocation[0];
     }
 }
