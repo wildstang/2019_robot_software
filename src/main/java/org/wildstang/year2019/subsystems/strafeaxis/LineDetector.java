45d068b3fad2037d8d54a5de3dc3dd1dc958f994
Wuump <39094532+Wuump@users.noreply.github.com>
2019-03-07 14:19:43 -0600

    git

--- src/main/java/org/wildstang/year2019/subsystems/strafeaxis/LineDetector.java
+++ src/main/java/org/wildstang/year2019/subsystems/strafeaxis/LineDetector.java
@@ -2,6 +2,12 @@
 
 package org.wildstang.year2019.subsystems.strafeaxis;
 
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.nio.file.*;
+import java.io.*;
+import java.sql.Driver;
+
 import javax.sound.sampled.Line;
 
 import edu.wpi.first.wpilibj.*;
@@ -14,86 +20,121 @@ NA - 06 - 05 - 04 - 03 - 02 - 01 - 07 - 15 - 14 - 13 - 12 - 11 - 10 - 09 - 08
 */
 
 public class LineDetector extends Thread {
+  private DriverStation ds;
   private SerialPort arduino;
+  private FileOutputStream out;
+  private byte pass[] = {10, (byte)150, 58, 9};
+  private int matchtime = 150;
+
+  private String filename; 
+
   private static int[] SENSOR_CONSTANTS = { -130, -114, 96, -82, -56, -40, -24, -8, 0, 8, 24, 40, 56, 82, 96, 114,
       130 };
   private static double TICKS_PER_MM = 17.746;
   boolean arduinoActive;
   // private byte[] lineLocation = new byte[1];
-  private byte[] lineData = new byte[16];
+  private byte[] lineData = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
   private byte linePosition; 
   public static double AVERAGE_CONSTANSTS[] = new double[16];
   public double average[] = new double[16];
   public double offFromAverage[] = new double[16];
+  public boolean ArduinoActive = false; 
+  
 
-  public LineDetector() {
+
+  public LineDetector() throws NullPointerException {   
+    
+    try {
+      File file = new File("/home/lvuser/log");
+      file.createNewFile();
+      out = new FileOutputStream(file, false);
+      out.write(pass);
+      out.write(pass);
+      System.out.println("file created") ;
+    }
+    catch(IOException ex) {
+      System.out.println("file not created");
+      System.out.println(ex);
+    }
 
     try {
       arduino = new SerialPort(9600, Port.kUSB);
       System.out.println("Connected to kUSB");
+      arduinoActive = true;
     } catch (Exception e) {
       System.out.println("Falied to connect to kUSB.  Attempting to connect to kUSB1");
       try {
         arduino = new SerialPort(9600, Port.kUSB1);
         System.out.println("Connected to kUSB1");
+        arduinoActive = true;
       } catch (Exception e1) {
         System.out.println("Falied to connect to kUSB1.  Attempting to connect to kUSB2");
         try {
           arduino = new SerialPort(9600, Port.kUSB2);
           System.out.println("Connected to kUSB2");
+          arduinoActive = true;
         } catch (Exception e2) {
           System.out.println("Failed to connect");
         }
       }
     }
-    arduino.setReadBufferSize(1);
-  }
-
-  public void run() {
-    while (true) {
-      byte valueRead = arduino.read(1)[0];
-      /*
-       * We keep reading until we get a byte with the high bit set. In Java, byte is
-       * signed so if the high bit is set the valueRead will be <0.
-       */
-      while (valueRead >= 0) {
-        valueRead = arduino.read(1)[0];
-      }
-      lineData[0] = (byte) ((int) valueRead + 128);
-      for (int i = 1; i < lineData.length; ++i) {
-        lineData[i] = arduino.read(1)[0];
-      }
-      // System.out.println(lineLocation[0]);
-      // return SENSOR_CONSTANTS[lineLocation[0]] * TICKS_PER_MM;
+    try {
+      arduino.setReadBufferSize(1);
     }
-  }
+    catch(NullPointerException e) {
 
-  public void setAverage() {
-    System.arraycopy(lineData, 0, average, 0, lineData.length);
-    int loopDuration = 32;
-    while(loopDuration > 0) {
-      for(int i = 0; i < 16; i ++) {
-        average[i] = (average[i] + lineData[i]) / 2;
-      }
-      loopDuration++;
     }
     
-  };
 
+    DriverStation ds = DriverStation.getInstance();
 
-  public int getLineSensor() {
-    byte percievedLine = 0;
+    
+  }
 
-    for(byte i = 0; i < 16; i ++) {
-      if (Math.abs(lineData[percievedLine] - average[percievedLine]) < Math.abs(lineData[percievedLine] - average[percievedLine] )) {
-        percievedLine = i; 
+  
+  public void run()  {
+    if(arduinoActive) {
+      while (true) {
+        byte valueRead = arduino.read(1)[0];
+        
+         //We keep reading until we get a byte with the high bit set. In Java, byte is
+         //signed so if the high bit is set the valueRead will be <0.
+         
+        while (valueRead >= 0) {  
+          valueRead = arduino.read(1)[0];
+        }
+        lineData[0] = (byte) ((int) valueRead + 128);
+        for (int i = 1; i < lineData.length; ++i) {
+          lineData[i] = arduino.read(1)[0];
+        }
       }
     }
 
-    return SENSOR_CONSTANTS[percievedLine];
+    
+    
+     
+      /*
+      do {
+        //wait
+      } while(matchtime != (int)ds.getMatchTime());
+      try {
+        for(byte data : lineData) {
+          out.write(data);
+          out.write(10);
+        }
+        out.write(pass);
+        matchtime = (int)ds.getMatchTime();
+        pass[1] = (byte)matchtime;
+        
+      }
+      catch(IOException ex) {}
+      
+      matchtime--;
+      */
+    
+    
   }
-
-  public byte[] getLineSensorData() {
+  public byte[] getLineSensorData() throws NullPointerException {
     return lineData;
   }
 
@@ -101,5 +142,7 @@ public class LineDetector extends Thread {
    * 130 114 96 82 56 40 24 08 0 08 24 40 56 82 96 114 130 NA - 06 - 05 - 04 - 03
    * - 02 - 01 - 07 - 15 - 14 - 13 - 12 - 11 - 10 - 09 - 08 C
    */
+
+   
 
 }
