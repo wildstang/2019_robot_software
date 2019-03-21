cd505885a915e1370c0399e192529fbf07176efa
smccrorie <42500981+smccrorie@users.noreply.github.com>
2019-03-20 19:44:14 -0500

    Things to make it work with 2018 robot I think, needs testing

--- src/main/java/org/wildstang/year2019/auto/programs/ExampleAutoProgram.java
+++ src/main/java/org/wildstang/year2019/auto/programs/ExampleAutoProgram.java
@@ -1,19 +1,19 @@
-package org.wildstang.year2019.auto.programs;
+// package org.wildstang.year2019.auto.programs;
 
-import org.wildstang.framework.auto.AutoProgram;
-import org.wildstang.year2019.auto.steps.ExampleAutoStep;
+// import org.wildstang.framework.auto.AutoProgram;
+// import org.wildstang.year2019.auto.steps.ExampleAutoStep;
 
-public class ExampleAutoProgram extends AutoProgram {
+// public class ExampleAutoProgram extends AutoProgram {
 
-    @Override
-    protected void defineSteps() {
-        addStep(new ExampleAutoStep());
-    }
+//     @Override
+//     protected void defineSteps() {
+//         addStep(new ExampleAutoStep());
+//     }
 
-    @Override
-    public String toString() {
-        //give it a name
-        return "ExampleAutoProgram";
-    }
+//     @Override
+//     public String toString() {
+//         //give it a name
+//         return "ExampleAutoProgram";
+//     }
 
-}
+// }
