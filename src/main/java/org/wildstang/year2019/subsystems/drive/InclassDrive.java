09c2377c35c07eb99dfc96dde24cf1b7a5a8acf2
Liam Fruzyna <liamfruzyna@gmail.com>
2019-11-10 19:39:09 -0600

    In class project for drive systems

---
+++ src/main/java/org/wildstang/year2019/subsystems/drive/InclassDrive.java
@@ -0,0 +1,93 @@
+package org.wildstang.year2019.subsystems.drive;
+
+/**
+ * Class:       InclassDrive.java
+ * Inputs:      2 joysticks and 1 button
+ * Outputs:     4 talons
+ * Description: This is the starting class for an in-class project on Nov 11 2019.
+ *              Assumes a robot with a simple 4 wheel drive, 2 on each side, with a Talon to control each wheel.
+ * 
+ *              You, the programmer, are given the 2 joysticks to work with plus one button for additional feature
+ *              control. With these you need to create various drive systems to control the robot. Do not add any
+ *              additional input or outputs. Create a new class using this template for parts 1, 3, and 5.
+ * 
+ *              There are X parts to this project. Try them one at a time:
+ *              1:  Build a tank drive system where the Y-Axis of each joystick controls the speed of the wheels
+ *                  on the matching side of the robot.
+ *              2:  Implement an anti-turbo button that runs the robot at quarter speed for increased precision.
+ *              3:  Build a halo drive system where the Left Y-Axis controls the speed of the robot and the Right
+ *                  X-Axis controls the direction the robot turns (proportion of the speed left-right).
+ *              4:  Implement quick turn functionality where the robot will turn faster when a button is pressed
+ *                  or the right joystick reaches -1/1 (turn faster by running one side in reverse).
+ *              5:  Build a holonomic drive system controlled by arcade drive where there is an angled wheel on each
+ *                  corner of the robot that is controled with a single joystick by following the direction it is pointing.
+ * 
+ *              Note: You aren't expected to complete the whole project in one class, nor does it need to be completed
+ *              and turned in. If you do not complete the project today, feel free to work on it in your free time,
+ *              we may or may not come back to it in a future class.
+ */
+public class InclassDrive implements Subsystem {
+
+    // inputs
+    AnalogInput leftXAxis;
+    AnalogInput leftYAxis;
+    AnalogInput rightXAxis;
+    AnalogInput rightYAxis;
+    DigitalInput featureButton;
+
+    // outputs
+    TalonSRX leftFrontDrive;
+    TalonSRX rightFrontDrive;
+    TalonSRX leftRearDrive;
+    TalonSRX rightRearDrive;
+
+    @Override
+    public void resetState() {
+        // set the default state of any variables you create here
+    }
+
+    @Override
+    public void inputUpdate(Input source) {
+        // respond to changes for the various inputs here
+    }
+
+    @Override
+    public void update() {
+        // update the outputs here
+    }
+
+    @Override
+    public void init() {
+        // initialize inputs
+        featureButton = (DigitalInput) Core.getInputManager().getInput(WSInputs.ANTITURBO.getName());
+        featureButton.addInputListener(this);
+
+        leftXAxis = (AnalogInput) Core.getInputManager().getInput(WSInputs.);
+        leftXAxis.addInputListener(this);
+
+        leftYAxis = (AnalogInput) Core.getInputManager().getInput(WSInputs.);
+        leftYAxis.addInputListener(this);
+
+        rightXAxis = (AnalogInput) Core.getInputManager().getInput(WSInputs.);
+        rightXAxis.addInputListener(this);
+
+        rightYAxis = (AnalogInput) Core.getInputManager().getInput(WSInputs.);
+        rightYAxis.addInputListener(this);
+
+        // initialize outputs
+        leftFrontDrive = new TalonSRX(CANConstants.)
+
+        rightFrontDrive = new TalonSRX(CANConstants.)
+        rightFrontDrive.setInverted(true);
+
+        leftRearDrive = new TalonSRX(CANConstants.)
+
+        rightRearDrive = new TalonSRX(CANConstants.)
+        rightRearDrive.setInverted(true);
+
+        resetState();
+    }
+
+    @Override public String getName() { return "Inclass Drive Assignment"; }
+    @Override public void  selfTest() { }
+}
