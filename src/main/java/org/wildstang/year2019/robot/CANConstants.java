ee1ea210d0c1af24149adbd79e66800448de11b8
Dominic Ritchey <me@ritcheydominic.com>
2020-01-09 20:20:06 -0600

    Partially change Drive to emulate 2018 drive base
    It's not working so it needs some debugging.

--- src/main/java/org/wildstang/year2019/robot/CANConstants.java
+++ src/main/java/org/wildstang/year2019/robot/CANConstants.java
@@ -1,11 +1,12 @@
 package org.wildstang.year2019.robot;
 
 public final class CANConstants {
-    public static final int LEFT_DRIVE_TALON = 3;
-    public static final int RIGHT_DRIVE_TALON = 6;
+    public static final int[] LEFT_DRIVE_TALONS = {2, 3};
+    public static final int[] RIGHT_DRIVE_TALONS = {4, 13};
 
-    public static final int[] LEFT_DRIVE_VICTORS = {1,2};
-    public static final int[] RIGHT_DRIVE_VICTORS = {4,5};
+    // TESTING: Some Victors removed to emulate 6-wheel drive
+    public static final int LEFT_DRIVE_VICTOR = 1;
+    public static final int RIGHT_DRIVE_VICTOR = 5;
 
     // TODO put in correct IDs
     public static final int STRAFE_TALON = 12;//this is a talon right?
